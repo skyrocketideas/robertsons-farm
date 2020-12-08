54cb7d551f6fc7482b45dba30b13e22291081f08
graeme <grae0026@stud.kea.dk>
2020-12-08 13:47:29 +0100

    feature: add sections to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,21 @@ function Home() {
 	return (
 		<div>
 			<h1>Home</h1>
+			<section className="home-section">
+				<h2>First Section</h2>
+			</section>
+			<section className="home-section">
+				<h2>Latest Articles</h2>
+			</section>
+			<section className="home-section">
+				<h2>Shop</h2>
+			</section>
+			<section className="home-section">
+				<h2>Latest Products</h2>
+			</section>
+			<section className="home-section">
+				<h2>Mailing List</h2>
+			</section>
 		</div>
 	);
 }
