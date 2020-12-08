c087136a59d04a70e8e61ed22c9a45b4b324e68c
graeme <grae0026@stud.kea.dk>
2020-12-08 13:04:42 +0100

    feature: add article template to blog page

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,20 +6,7 @@ import "../styles/header.css";
 function Header() {
 	return (
 		<div className="header-container">
-			<div className="article">
-				<div className="article__feat-img"></div>
-				<div className="article__text-wrapper">
-					<h2 className="article__category">Category</h2>
-					<h1 className="article__title">Title of article</h1>
-					<p className="article__excerpt">
-						Excerpt Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Nulla provident aut ullam dolorum sit impedit fugit similique rerum
-						omnis, quod dicta, voluptas eius animi quae reiciendis possimus
-						voluptate molestias dolore.
-					</p>
-					<Button />
-				</div>
-			</div>
+			<h1>Header</h1>
 		</div>
 	);
 }
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,10 +1,25 @@
 import React from "react";
+import Button from "../components/Button";
 import "../App.css";
 
 function Blog() {
 	return (
 		<div>
 			<h1>Blog</h1>
+			<article className="blog-post">
+				<div className="blog-post__feat-img"></div>
+				<div className="blog-post__text-wrapper">
+					<h2 className="blog-post__category">Category</h2>
+					<h1 className="blog-post__title">Title of blog-post</h1>
+					<p className="blog-post__excerpt">
+						Excerpt Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Nulla provident aut ullam dolorum sit impedit fugit similique rerum
+						omnis, quod dicta, voluptas eius animi quae reiciendis possimus
+						voluptate molestias dolore.
+					</p>
+					<Button />
+				</div>
+			</article>
 		</div>
 	);
 }
