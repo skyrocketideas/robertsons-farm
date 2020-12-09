d0e910bd92dae7e309c5aa27f602f24ee3ba6ece
graeme <grae0026@stud.kea.dk>
2020-12-09 12:04:25 +0100

    feature: add featured image to blog post

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -9,12 +9,25 @@ const ArticleList = function (props) {
 		<ul>
 			{repos.map(function (repo) {
 				return (
-					<li key={repo.id}>
-						<h3
+					<li key={repo.id} className="blog-post">
+						<div
+							className="blog-post__feat-img"
+							style={{
+								backgroundImage: `url(${repo._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url})`,
+							}}
+						></div>
+						<h2 className="blog-post__category">
+							{repo._embedded["wp:term"][0][0].name}
+						</h2>
+						<h1
 							dangerouslySetInnerHTML={{
 								__html: `${repo.title.rendered}`,
 							}}
-						></h3>
+						></h1>
+						<h3 className="blog-post__author">
+							{repo._embedded["author"][0].name}
+						</h3>
+						<h4 className="blog_post__date">{repo.date}</h4>
 					</li>
 				);
 			})}
@@ -23,3 +36,18 @@ const ArticleList = function (props) {
 };
 
 export default ArticleList;
+
+// <article className="blog-post">
+// 	<div className="blog-post__feat-img"></div>
+// 	<div className="blog-post__text-wrapper">
+// 		<h2 className="blog-post__category">Category</h2>
+// 		<h1 className="blog-post__title">Title of blog-post</h1>
+// 		<p className="blog-post__excerpt">
+// 			Excerpt Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+// 			provident aut ullam dolorum sit impedit fugit similique rerum omnis, quod
+// 			dicta, voluptas eius animi quae reiciendis possimus voluptate molestias
+// 			dolore.
+// 		</p>
+// 		<Button />
+// 	</div>
+// </article>;
