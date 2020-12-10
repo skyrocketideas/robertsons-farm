bfd3be5816671a803fd9e3348b0181761b64c1d7
graeme <grae0026@stud.kea.dk>
2020-12-10 16:36:39 +0100

    fix: add comment for api query

--- src/modules/Database.js
+++ src/modules/Database.js
@@ -1,3 +1,4 @@
+/* API query for feauture sticky post */
 const GetFeaturedArticle = async () => {
 	const featuredArticle = await fetch(
 		"https://lovethatwillnotdie.com/wp_robertsons/wp-json/wp/v2/posts/?_embed&sticky=true"
