2e3b312f2f1a19c77e3ba1b090728b6da6c2eb71
graeme <grae0026@stud.kea.dk>
2020-12-15 15:34:25 +0100

    feature: add button component to mailing list

--- src/components/MailingList.js
+++ src/components/MailingList.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Button } from "../components/Button";
 import "../App.css";
 
 function MailingList() {
 	return (
 		<div>
 			<h1>Mailing List</h1>
+			<h2>Sign up to get the latest updates from Robertson's Farm</h2>
+			<Button
+				onClick=""
+				buttonStyle="btn__secondary--outline"
+				type="button"
+				value="send"
+			>
+				Sign Up
+			</Button>
 		</div>
 	);
 }
