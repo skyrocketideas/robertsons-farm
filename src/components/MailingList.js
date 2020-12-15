b3001514348f5d48a92e29540156b4b8d2a01ee3
graeme <grae0026@stud.kea.dk>
2020-12-15 15:48:48 +0100

    feature: add form fields

--- src/components/MailingList.js
+++ src/components/MailingList.js
@@ -4,9 +4,31 @@ import "../App.css";
 
 function MailingList() {
 	return (
-		<div>
-			<h1>Mailing List</h1>
-			<h2>Sign up to get the latest updates from Robertson's Farm</h2>
+		<div className="mailing-list">
+			<section className="mailing-list__intro">
+				<h1>Mailing List</h1>
+				<h2>Sign up to get the latest updates from Robertson's Farm</h2>
+			</section>
+			<section className="mailing-list__form">
+				<form action="" className="mailing-list__form__inputs">
+					<label htmlFor="first name">First name</label>
+					<input
+						type="text"
+						name="first name"
+						placeholder="First name"
+						required
+					/>
+					<label htmlFor="last name">Last name</label>
+					<input
+						type="text"
+						name="last name"
+						placeholder="Last name"
+						required
+					/>
+					<label htmlFor="email">Email</label>
+					<input type="email" name="email" placeholder="Email" required />
+				</form>
+			</section>
 			<Button
 				onClick=""
 				buttonStyle="btn__secondary--outline"
