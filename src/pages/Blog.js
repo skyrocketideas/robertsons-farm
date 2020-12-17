e2ea83e69b44024cc545832dc363b1ce9d9eca21
graeme <grae0026@stud.kea.dk>
2020-12-17 16:57:01 +0100

    fix: article container grid

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ArticleListData from "../components/ArticleListData";
-
 import "../App.css";
+import "../styles/blog.css";
 
 function Blog() {
 	return (
 		<div>
-			<h1>Blog</h1>
+			<h1>Articles</h1>
 			<ArticleListData />
 		</div>
 	);
