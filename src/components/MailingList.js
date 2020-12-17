ac2f868935aa31b8220c8f08e460def9ab455358
graeme <grae0026@stud.kea.dk>
2020-12-17 10:24:16 +0100

    fix: form styling home page

--- src/components/MailingList.js
+++ src/components/MailingList.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Button } from "../components/Button";
 import { Formik, Field, Form } from "formik";
 import "../App.css";
+import "../styles/mailingList.css";
 
 function MailingList() {
 	return (
 		<div className="mailing-list">
 			<section className="mailing-list__intro">
-				<h1>Mailing List</h1>
-				<h2>Sign up to get the latest updates from Robertson's Farm</h2>
+				<h3>Sign up to get the latest updates from Robertson's Farm</h3>
 			</section>
 			<section className="mailing-list__form">
 				<Formik
@@ -18,24 +18,35 @@ function MailingList() {
 						email: "",
 					}}
 					onSubmit={async (values) => {
-                        await new Promise((r) => setTimeout(r, 500));
-                        alert(JSON.stringify(values, null, 2));
+						await new Promise((r) => setTimeout(r, 500));
+						alert(JSON.stringify(values, null, 2));
 					}}
 				>
 					<Form>
-						<label htmlFor="firstName">First name</label>
-						<Field id="firstName" name="firstName" placeholder="Jane" />
-
-						<label htmlFor="lastName">Last name</label>
-						<Field id="lastName" name="lastName" placeholder="Doe" />
-
-						<label htmlFor="email">Email</label>
-						<Field
-							id="email"
-							name="email"
-							placeholder="jane@acme.com"
-							type="email"
-						/>
+						<div className="form-inputs-group">
+							<label htmlFor="firstName">First name</label>
+							<Field
+								id="firstName"
+								className="form-field"
+								name="firstName"
+								placeholder="Jane"
+							/>
+							<label htmlFor="lastName">Last name</label>
+							<Field
+								id="lastName"
+								className="form-field"
+								name="lastName"
+								placeholder="Doe"
+							/>
+							<label htmlFor="email">Email</label>
+							<Field
+								id="email"
+								className="form-field"
+								name="email"
+								placeholder="jane@acme.com"
+								type="email"
+							/>
+						</div>
 						<Button buttonStyle="btn__secondary--outline" type="submit">
 							Submit
 						</Button>
