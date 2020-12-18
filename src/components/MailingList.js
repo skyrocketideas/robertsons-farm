5089ef5eeaae7476acf1ec7e83c662e1f389bd9f
graeme <grae0026@stud.kea.dk>
2020-12-18 09:14:51 +0100

    fix: mailing list message

--- src/components/MailingList.js
+++ src/components/MailingList.js
@@ -19,7 +19,7 @@ function MailingList() {
 					}}
 					onSubmit={async (values) => {
 						await new Promise((r) => setTimeout(r, 500));
-						alert(JSON.stringify(values, null, 2));
+						alert("thanks!");
 					}}
 				>
 					<Form>
